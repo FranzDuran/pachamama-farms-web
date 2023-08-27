4e108b7a35e97c9a7a4667dbd0ab90ad648e8bf1
FranzDuran <franzduran.fd@gmail.com>
2023-08-26 23:12:14 -0300

    secciones actualizadas

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-import HomeSeccion from "@/app/seccions/home/home.js"
-import AboutUs from "./seccions/aboutUs/aboutUs"
+import HomeSeccion from "@/app/seccions/home/home.js";
+import AboutUs from "./seccions/aboutUs/aboutUs";
 import Navbar from "@/components/Navbar/Navbar";
 import styles from "./page.module.css";
 import ContactUs from "./seccions/ContactUs/ContactUs";
-// import HomeSeccion from "@/app/seccions/home/home.js";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <HomeSeccion></HomeSeccion> 
+      <HomeSeccion></HomeSeccion>
       <AboutUs></AboutUs>
+      <ContactUs />
 
       {/* <section data-text-color="white" className={styles.section1}>
         <h1>Section 1</h1>
@@ -19,8 +19,7 @@ export default function Home() {
         <h1>Section 2</h1>
       </section>
       <section data-text-color="white" className={styles.section3}>
-    <h1>Section 3</h1> */}
-    <ContactUs/>
+      <h1>Section 3</h1> */}
     </main>
   );
 }
