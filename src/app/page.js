98cbf02d407114d108aae389fd841615e25689b0
alej-mocse1 <alejandromocse01@gmail.com>
2023-08-27 14:08:32 -0300

    3ra seccion fruit lista, falta el feedback de dani

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,19 @@ import AboutUs from "./seccions/aboutUs/aboutUs";
 import Navbar from "@/components/Navbar/Navbar";
 import styles from "./page.module.css";
 import ContactUs from "./seccions/ContactUs/ContactUs";
+import FruitsSeccion from "./seccions/fruits/fruits";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <HomeSeccion></HomeSeccion>
-      <AboutUs></AboutUs>
+      <HomeSeccion/>
+      <AboutUs/>
+      <FruitsSeccion/>
       <ContactUs />
 
-      {/* <section data-text-color="white" className={styles.section1}>
-        <h1>Section 1</h1>
-      </section>
-      <section data-text-color="black" className={styles.section2}>
-        <h1>Section 2</h1>
-      </section>
-      <section data-text-color="white" className={styles.section3}>
-      <h1>Section 3</h1> */}
+
+
     </main>
   );
 }
