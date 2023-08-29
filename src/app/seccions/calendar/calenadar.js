24b04fedd3da45146d4b0ea8812621b54dd76048
alej-mocse1 <alejandromocse01@gmail.com>
2023-08-29 12:18:37 -0300

    popup video Yt termindo

--- src/app/seccions/calendar/calenadar.js
+++ src/app/seccions/calendar/calenadar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './calendar.module.css'
 import Image from "next/image";
-import CalendarImg from './assets/calendar.png'
+import CalendarImg from './assets/CalendarInfo.png'
 
 const Calendar = () => {
 
@@ -9,7 +9,14 @@ const Calendar = () => {
     return(
        <section className={styles.sectionCalendar}>
         <div className={styles.divFondo}>
-          <div className={styles.divCalendar}></div>    
+          <div className={styles.divCalendar}>
+           <Image 
+            src={CalendarImg}
+            alt=""
+            title=""
+            className={styles.CalendarInfoIMG}
+           /> 
+          </div>    
         </div>    
        </section>
     )
