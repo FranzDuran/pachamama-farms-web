a9d232367b0f2d23fce9b01ce94f1ebd61603d51
alej-mocse1 <alejandromocse01@gmail.com>
2023-09-03 18:21:51 -0300

    calendario responsive

--- src/app/seccions/calendar/calenadar.js
+++ src/app/seccions/calendar/calenadar.js
@@ -8,35 +8,35 @@ import calendarResponsive from './assets/calendario_mobile.png'
 const Calendar = () => {
   return (
     <section className={styles.sectionCalendar} id="calendar">
-      <section className={styles.divFondo} data-text-color="white">
+      <article className={styles.divFondo} data-text-color="white">
         <Image src={imgfondo} alt="" title="" className={styles.fondoImg} />
         <div className={styles.divCalendar}>
           <Image
             src={calendar}
-            alt=""
-            title=""
+            alt="calendar showing the months in which all fruits are grown"
+            title="crop calendar"
             className={styles.calendarImg}
           />
           
           <Image
             src={CalendarImg}
-            alt=""
-            title=""
+            alt="seasonal calendar where all fruits are grown"
+            title="season calendar"
             className={styles.CalendarInfoIMG}
           />
 
             <Image
             src={calendarResponsive}
-            alt=""
-            title=""
+            alt="responsive crop calendar"
+            title="Calendar showing the months in which all responsive fruits are grown"
             className={styles.CalendarResponsive}
           />
         </div>
-      </section>
-      <section
+      </article>
+      <div
         className={styles.sectionWhite}
         data-text-color="black"
-      ></section>
+      ></div>
     </section>
   );
 };
