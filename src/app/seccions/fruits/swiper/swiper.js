1d21a43d52aab39379129a108b3e72ab3d39d1ef
FranzDuran <franzduran.fd@gmail.com>
2023-09-05 13:42:40 -0300

    flechas nav

--- src/app/seccions/fruits/swiper/swiper.js
+++ src/app/seccions/fruits/swiper/swiper.js
@@ -1,3 +1,4 @@
+"use client";
 // import Swiper core and required modules
 import { Navigation, Pagination, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,15 +10,43 @@ import img3 from "../assets/6.jpg";
 import img4 from "../assets/mobile4.jpg";
 import img5 from "../assets/mobile5.jpg";
 import img6 from "../assets/mobile6.jpg";
+import { useEffect, useState } from "react";
 
 // Import Swiper stylesjj
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "../../OurPeople/OurPeople.css"
 
 
 export default function SwiperCarrousel (){
+
+  const handleWindowResize = () => {
+    // Aquí defines el ancho máximo para la versión móvil
+    const mobileMaxWidth = 920;
+    const shouldShowNavigation = window.innerWidth > mobileMaxWidth;
+
+    // Actualiza el estado para mostrar u ocultar la navegación
+    setShowNavigation(shouldShowNavigation);
+  };
+
+  useEffect(() => {
+    // Agrega un event listener para el cambio de tamaño de la ventana
+    window.addEventListener("resize", handleWindowResize);
+
+    // Llama a la función una vez al inicio para configurar inicialmente el estado
+    handleWindowResize();
+
+    // Limpia el event listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const [showNavigation, setShowNavigation] = useState(true);
+
+
   return (
     <div className="container">
       <Swiper
@@ -57,6 +86,24 @@ export default function SwiperCarrousel (){
             spaceBetween: 0,
           },
         }}
+
+        navigation={
+          showNavigation
+            ? {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              }
+            : false
+        }
+        pagination={
+          !showNavigation
+            ? {
+                clickable: true,
+                el: ".paginationSwiper",
+              }
+            : false
+        }
+        onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
           <article className={styles.divContenido}>
@@ -95,6 +142,8 @@ export default function SwiperCarrousel (){
           </article>
         </SwiperSlide>
       </Swiper>
+      <button className="swiper-button-next"></button>
+      <button className="swiper-button-prev"></button>
     </div>
   );
 };
